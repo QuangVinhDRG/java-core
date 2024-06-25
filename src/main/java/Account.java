5637f3b4ef20c1e5ca8a2693ef3fb6addcf8df7b
QuangVinh <bloodmoon.251103@gmail.com>
2024-06-25 14:47:17 +0700

    Update password property

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -3,7 +3,8 @@ import java.io.Serializable;
 public class Account implements Serializable {
     private int id;
     private String username;
-    private transient String password; // transient: không lưu thuộc tính này trong file
+    //    private transient String password; // transient: không lưu thuộc tính này trong file
+    private String password;
 
     public Account(int id, String username, String password) {
         this.id = id;
@@ -13,10 +14,6 @@ public class Account implements Serializable {
 
     @Override
     public String toString() {
-        return "Account{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+        return String.format("%d, %s, %s\n", id, username, password);
     }
 }
